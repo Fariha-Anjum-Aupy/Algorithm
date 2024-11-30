9d46b0e1218c1dbd2e5c6fd4dcbb4946da4a1323
noobfornow <farihaanjumaupy@gmail.com>
2024-12-01 00:47:40 +0600

    second commit

--- lab 3/test-cases.cpp
+++ lab 3/test-cases.cpp
@@ -15,7 +15,7 @@ int main() {
     }
 
     // Array sizes to generate
-    vector<int> sizes = {1000, 5000, 10000, 50000};
+    vector<int> sizes = {1000, 5000, 10000, 50000, 100000};
 
     // Number of test cases
     int t = sizes.size();
